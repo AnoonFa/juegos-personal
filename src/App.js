bba4625f2d5d6e07feacebdcc8d9ca41d68f0a0f
ElPinasXD <estebanpineros42@gmail.com>
2024-08-11 19:20:02 -0500

    ocmponentes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import FilledAlerts from './components/MuiComponents/Alerts';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FilledAlerts />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
